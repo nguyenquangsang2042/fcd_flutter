b6c5ee90af8dd0e3a7ad9cf1f29320335280f3b7
sangnq <sangnq@vuthao.com>
2023-05-26 16:39:59 +0700

    add textfield

--- lib/screens/login_mail.dart
+++ lib/screens/login_mail.dart
@@ -5,28 +5,50 @@ class LoginMailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('asset/images/Background.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 30,),
-          Container(
-              child: Image.asset('asset/images/vna_logo.png')
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('asset/images/Background.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-          // Center empty container
-          Expanded(
-            child: Container(),
-          ),
-          Container(
-           child: Text("B"),
-          ),
-        ],
-      )
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset('asset/images/vna_logo.png'),
+                  const TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: "Input your email of VietnamAirlines",
+                      hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.normal
+                      ),
+                      // add any other decoration properties you want
+                    ),
+                  )
+                ],
+              )),
+              // Center empty container
+              Expanded(
+                child: Container(),
+              ),
+              Container(
+                child: Text(" sB"),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          )),
     );
   }
 }
