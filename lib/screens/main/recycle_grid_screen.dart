ac6cb2015f0c11d3915f79d652ed20c4d35e612b
sangnq2903 <34269455+nguyenquangsang2042@users.noreply.github.com>
2023-06-13 21:42:09 +0700

    add redirect to notify screen

--- lib/screens/main/recycle_grid_screen.dart
+++ lib/screens/main/recycle_grid_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fcd_flutter/base/constanst.dart';
+import 'package:fcd_flutter/screens/notification/notification_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_grid_list/responsive_grid_list.dart';
 
@@ -25,37 +26,45 @@ class RecycleGridScreen extends StatelessWidget {
               listViewBuilderOptions:
                   ListViewBuilderOptions(), // Options that are getting passed to the ListView.builder() function
               children: snapshot.data!
-                  .map((e) => Container(
-                        height: 110,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF00485C),
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
+                  .map((e) => InkResponse(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => redirectToView(e.key)));
+                        },
+                        child: Container(
+                          height: 110,
+                          decoration: BoxDecoration(
                             color: const Color(0xFF00485C),
-                            width: 10,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: const Color(0xFF00485C),
+                              width: 10,
+                            ),
                           ),
-                        ),
-                        child: Center(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 50,
-                                  width: 70,
-                                  child: Image.asset(
-                                      'asset/images/${pathImage(e.key)}.png'),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  e.title,
-                                  style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                )
-                              ]),
+                          child: Center(
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    height: 50,
+                                    width: 70,
+                                    child: Image.asset(
+                                        'asset/images/${pathImage(e.key)}.png'),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    e.title,
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  )
+                                ]),
+                          ),
                         ),
                       ))
                   .toList()
@@ -96,6 +105,37 @@ class RecycleGridScreen extends StatelessWidget {
         return 'ic_menu_application';
       default:
         return 'icon_shield2';
+    }
+  }
+  
+  redirectToView(String key) {
+    switch (key) {
+      case 'Safety':
+        return NotificationScreen();
+      case 'News':
+        return 'ic_news';
+      case 'Licence':
+        return 'icon_lisence30';
+      case 'Schedule':
+        return 'icon_plane30Gray';
+      case 'Ticket request':
+        return 'icon_ticket_booking30';
+      case 'Training':
+        return 'icon_training';
+      case 'Payroll':
+        return 'icon_payroll';
+      case 'Library':
+        return 'icon_library30';
+      case 'Contacts':
+        return 'icon_user2';
+      case 'FAQs':
+        return 'icon_FAQs';
+      case 'Report':
+        return 'icon_report';
+      case 'Application':
+        return 'ic_menu_application';
+      default:
+        return NotificationScreen();
     }
   }
 }
