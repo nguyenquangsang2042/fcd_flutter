389ae4fba8fb02cf3228c475b27e3b7e2ebc40b5
sangnq <sangnq@vuthao.com>
2023-06-16 17:44:05 +0700

    add refresh

--- lib/screens/notification/notification_screen.dart
+++ lib/screens/notification/notification_screen.dart
@@ -2,6 +2,7 @@ import 'package:fcd_flutter/base/constanst.dart';
 import 'package:fcd_flutter/base/widgets/image_with_cookie.dart';
 import 'package:fcd_flutter/screens/notification/news_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class NotificationScreen extends StatelessWidget {
   NotificationScreen({super.key});
@@ -9,6 +10,7 @@ class NotificationScreen extends StatelessWidget {
   late List<String> defaultSafety;
   String keyNew = '';
   late String beanAnnounceID;
+  GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -87,89 +89,161 @@ class NotificationScreen extends StatelessWidget {
                   child: Row(
                     children: [
                       Expanded(
+                        flex: 1,
                         child:
                             TextButton(onPressed: () {}, child: Text('Safety')),
-                        flex: 1,
                       ),
                       Expanded(
+                        flex: 1,
                         child: TextButton(
                             onPressed: () {}, child: Text('Operation')),
-                        flex: 1,
                       ),
                     ],
                   ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: RefreshIndicator(
-                    onRefresh: ()async{
-                      Constanst.apiController.updateNation();
-                    }, child: StreamBuilder(
-                      stream: Constanst.db.notifyDao
-                          .getListNotifyWithAnnounceCategory(
-                          defaultSafety, keyNew),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData && snapshot.data != null) {
-                          return ListView.builder(
-                              itemCount: snapshot.data?.length,
-                              itemBuilder: (context, index) {
-                                return Container(child: InkResponse(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 7, right: 7, top: 5, bottom: 5),
-                                    child: ListTile(
-                                      leading: SizedBox(
-                                        height: 50,
-                                        width: 50,
-                                        child: ImageWithCookie(
-                                            imageUrl:
-                                            '${Constanst.baseURL}${snapshot.data![index].iconPath!}',
-                                            errImage:
-                                            'asset/images/logo_vna120.png'),
+                  child: LiquidPullToRefresh(
+                    height: 100,
+                    color: const Color(0xFF006784),
+                    showChildOpacityTransition: true,
+                    onRefresh: () async {
+                      Constanst.apiController.updateNotify();
+                    },
+                    child: StreamBuilder(
+                        stream: Constanst.db.notifyDao
+                            .getListNotifyWithAnnounceCategory(
+                                defaultSafety, keyNew),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData && snapshot.data != null) {
+                            return ListView.builder(
+                                itemCount: snapshot.data?.length,
+                                itemBuilder: (context, index) {
+                                  return Container(
+                                    color: index % 2 != 0
+                                        ? Colors.white
+                                        : Colors.blueGrey.shade50,
+                                    child: InkResponse(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 7,
+                                            right: 7,
+                                            top: 5,
+                                            bottom: 5),
+                                        child: ListTile(
+                                          leading: SizedBox(
+                                            height: 50,
+                                            width: 50,
+                                            child: ImageWithCookie(
+                                                imageUrl:
+                                                    '${Constanst.baseURL}${snapshot.data![index].iconPath!}',
+                                                errImage:
+                                                    'asset/images/logo_vna120.png'),
+                                          ),
+                                          title: Text(
+                                              snapshot.data![index].content),
+                                          subtitle: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Text(snapshot
+                                                  .data![index].content),
+                                              Row(
+                                                children: [
+                                                  Expanded(
+                                                    flex: 8,
+                                                    child: Text(snapshot
+                                                        .data![index].content),
+                                                  ),
+                                                  Expanded(
+                                                    flex: 2,
+                                                    child: Row(
+                                                      children: [
+                                                        if (snapshot
+                                                            .data![index]
+                                                            .flgSurvey)
+                                                          Expanded(
+                                                            flex: 1,
+                                                            child: SizedBox(
+                                                              height: 15,
+                                                              width: 15,
+                                                              child: Image.asset(
+                                                                  'asset/images/icon_reply.png'),
+                                                            ),
+                                                          ),
+                                                        if (snapshot
+                                                                .data![index]
+                                                                .flgReply &&
+                                                            !snapshot
+                                                                .data![index]
+                                                                .flgReplied)
+                                                          Expanded(
+                                                            flex: 1,
+                                                            child: SizedBox(
+                                                              height: 15,
+                                                              width: 15,
+                                                              child: Image.asset(
+                                                                  'asset/images/icon_answer.png'),
+                                                            ),
+                                                          ),
+                                                        if (snapshot
+                                                            .data![index]
+                                                            .flgConfirm)
+                                                          Expanded(
+                                                            flex: 1,
+                                                            child: SizedBox(
+                                                              height: 15,
+                                                              width: 15,
+                                                              child: Image.asset(
+                                                                  'asset/images/icon_confirm.png'),
+                                                            ),
+                                                          ),
+                                                        if (snapshot
+                                                            .data![index]
+                                                            .flgImmediately)
+                                                          Expanded(
+                                                            flex: 1,
+                                                            child: SizedBox(
+                                                              height: 15,
+                                                              width: 15,
+                                                              child: Image.asset(
+                                                                  'asset/images/icon_flaghigh.png'),
+                                                            ),
+                                                          ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                      title:
-                                      Text(snapshot.data![index].content),
-                                      subtitle: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          Text(snapshot.data![index].content),
-                                          Row(children: [
-                                            Expanded(child: Text(snapshot.data![index].content),flex: 8,),
-                                            Expanded(child:Row(children: [
-                                              if(snapshot.data![index].flgSurvey)
-                                                Expanded(child: SizedBox(height: 15,width: 15,child: Image.asset('asset/images/icon_reply.png'),),flex: 1,),
-                                              if(snapshot.data![index].flgReply &&!snapshot.data![index].flgReplied)
-                                                Expanded(child: SizedBox(height: 15,width: 15,child: Image.asset('asset/images/icon_answer.png'),),flex: 1,),
-                                              if(snapshot.data![index].flgConfirm)
-                                                Expanded(child: SizedBox(height: 15,width: 15,child: Image.asset('asset/images/icon_confirm.png'),),flex: 1,),
-                                              if(snapshot.data![index].flgImmediately)
-                                                Expanded(child:SizedBox(height: 15,width: 15,child:  Image.asset('asset/images/icon_flaghigh.png'),),flex: 1,),
-                                            ],),flex: 2,),
-                                          ],)
-                                        ],
-                                      ),
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    NewsScreen(
+                                                      notify:
+                                                          snapshot.data![index],
+                                                      safetyID:
+                                                          defaultSafety[0],
+                                                      qualificationID:
+                                                          defaultSafety[1],
+                                                    )));
+                                      },
                                     ),
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => NewsScreen(
-                                              notify: snapshot.data![index],
-                                              safetyID: defaultSafety[0],
-                                              qualificationID:
-                                              defaultSafety[1],
-                                            )));
-                                  },
-                                ),color: index%2!=0?Colors.white:Colors.blueGrey.shade50,);
-                              });
-                        } else {
-                          return const Center(
-                            child: Text("Không có dữ liệu"),
-                          );
-                        }
-                      }),
+                                  );
+                                });
+                          } else {
+                            return const Center(
+                              child: Text("Không có dữ liệu"),
+                            );
+                          }
+                        }),
                   ),
                 )
               ],
