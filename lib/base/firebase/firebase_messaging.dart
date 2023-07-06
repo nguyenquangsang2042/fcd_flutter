4e07bb329e373303f568f135de2d569c4d9e292e
sangnq2903 <34269455+nguyenquangsang2042@users.noreply.github.com>
2023-07-06 21:02:42 +0700

    chưa hiển thị push được.

--- lib/base/firebase/firebase_messaging.dart
+++ lib/base/firebase/firebase_messaging.dart
@@ -1,25 +1,55 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseMessagingService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   void initialize() async {
     _firebaseMessaging.requestPermission();
     await _firebaseMessaging.getToken();
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings();
+    final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print('onMessage: $message');
       // Xử lý thông báo đẩy khi ứng dụng đang mở
+      _firebaseMessagingBackgroundHandler(message);
     });
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('onMessageOpenedApp: $message');
       // Xử lý thông báo đẩy khi ứng dụng được khởi động từ trạng thái tắt
+      _firebaseMessagingBackgroundHandler(message);
+
     });
+
+  }
+  Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    print('onBackgroundMessage: $message');
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      channelDescription: 'channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: true,
+    );
+    const DarwinNotificationDetails iosPlatformChannelSpecifics = DarwinNotificationDetails();
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iosPlatformChannelSpecifics);
+
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      "data",
+      "data",
+      platformChannelSpecifics,
+      payload: "data",
+    );
   }
 
-  static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-    print('onBackgroundMessage: $message');
-    // Xử lý thông báo đẩy khi ứng dụng chạy trong nền
-  }
+
 
 }
